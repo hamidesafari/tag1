6cf22dc5f4589eadbfc85d62bb1e40be53327342
hamidesafari <hamidesafari606.gmail.com>
2021-09-13 15:28:10 +0200

    commit final

---
+++ aufgabe.js
@@ -0,0 +1,43 @@
+[3:47 PM] Michele Marschner
+    
+
+
+  
+  
+  
+    
+      
+      
+        Text
+      
+    
+    
+    
+      ### Recherche- und Theoriefragen
+1. Was ist der Unterschied zwischen ” == ” und ” === “ ? 
+2. Wozu dient in JS das Semikolon (;) ?
+### Praktische Coding-Aufgaben
+1. Welche Ausgaben erhälst du? Überlege erst und probiere es anschließend aus. Schreibe das Ergebnis neben die Aufgaben
+```
+var var1 = 5.0;
+var var2 = var1 + 2;
+var1 = 6;
+console.log(var1);                                   //Output:6
+console.log(var2);                                   //Output:6
+console.log(var1 + var2);                            //Output:7
+var2 = "Hallo";
+console.log(var1 + var2);                            //Output:6+Hallo
+console.log(var1 , var2);                            //Output:6Hallo
+console.log(11 % 3);                                 //Output:4
+var myString = "hello";
+var helloWorld = myString + " " + "world";
+console.log(helloWorld);                             //Output:
+```
+2. Deklariere eine Variable namens `name` und weise ihr einen String als Wert zu. Erstelle als nächstes eine zweite Variable namens `currentYear`, die als Wert das aktuelle Jahr zugewiesen bekommt. Weise einer dritten Variable namens `birthYear` dein Geburtsjahr zu. Als letztes deklariere eine Variable namens `age` und berechne dein Alter mit Hilfe der vorher erstellten Variablen `currentYear` und `birthYear`.
+    
+    
+  
+  
+
+
+<https://teams.microsoft.com/l/message/19:_vzd0kOGMUyIyguQBuUmNNJUfO1w_O77mURgXmvM-o81@thread.tacv2/1631281665408?tenantId=ab823e41-9aa7-4644-807b-6f215795a229&amp;groupId=0bfae67c-1846-4cb4-bb3c-caee994d0e93&amp;parentMessageId=1631281665408&amp;teamName=DCI MAT D03 A&amp;channelName=General&amp;createdTime=1631281665408>
---
+++ intro.js
@@ -0,0 +1,25 @@
+/*console.log("Hallo");
+console.log("5");
+
+console.log(typeof("Hallo"));
+console.log(typeof("5"));
+console.log("4");
+consol
+var var1 = 5.0;
+var var2 = var1 + 2;
+var1 = 6;*/
+var var1 = 5.0;
+var var2 = var1 + 2;
+var1 = 6;
+console.log(var1);
+var var1 = 5.0;
+var var2 = var1 + 2;
+var1 = 6;
+console.log(var1);                                   
+console.log(var2);                                   
+console.log(var1 + var2);                            
+var2 = "Hallo";
+console.log(var1 + var2);                            
+console.log(var1 , var2);                            
+console.log(11 % 3); 
+
